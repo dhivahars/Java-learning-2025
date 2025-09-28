56ef9ff7a08674f2bc3f9d8a924dfbcac6a0d148
dhivahars <dhivahars2003@gmail.com>
2025-09-28 16:26:07 +0530

    Streams Exercise-2

---
+++ src/PracticeCodes/firstWeek/streamsExcercise/eCommerce/Main.java
@@ -0,0 +1,43 @@
+package PracticeCodes.firstWeek.streamsExcercise.eCommerce;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static PracticeCodes.firstWeek.streamsExcercise.eCommerce.Status.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Orders> orders=new ArrayList<>();
+        orders.add(new Orders(1,"Martin",COMPLETED,10000));
+        orders.add(new Orders(2,"Shelby",PENDING,1000));
+        orders.add(new Orders(3,"Tim",CANCELLED,2500));
+        orders.add(new Orders(4,"Jersey",CANCELLED,5500));
+        orders.add(new Orders(5,"Dakota",PENDING,87834));
+        orders.add(new Orders(6,"Scarlett",COMPLETED,3200));
+        //Excercise-1
+        System.out.println("Completed Orders");
+        System.out.println("_______________________________________");
+        orders.stream().filter(a->a.getOrder_status()==COMPLETED)
+                .forEach(System.out::println);
+        //Excercise-2
+        System.out.println("\nTotal Revenue from Orders");
+        System.out.println("_______________________________________");
+        double sum=orders.stream().filter(a -> a.getOrder_status() == COMPLETED)
+                .mapToDouble(Orders::getAmount).sum();
+        System.out.println(sum);
+        //Excercise-3
+        System.out.println("\nCustomers Ordered more than 2000");
+        System.out.println("_______________________________________");
+        orders.stream().filter(a->a.getAmount()>2000).map(Orders::getCustomerName).forEach(System.out::println);
+        //Excercise-4
+        System.out.println("\nCancelled Orders count");
+        System.out.println("_______________________________________");
+        System.out.println(orders.stream().filter(a->a.getOrder_status()==CANCELLED).count());
+        //Excercise-5
+        System.out.println("\nOrder of Max Amount");
+        System.out.println("_______________________________________");
+        Orders max=orders.stream()
+                .max(Comparator.comparing(Orders::getAmount)).get();
+        System.out.println(max);
+    }
+}
---
+++ src/PracticeCodes/firstWeek/streamsExcercise/eCommerce/Orders.java
@@ -0,0 +1,55 @@
+package PracticeCodes.firstWeek.streamsExcercise.eCommerce;
+enum Status{COMPLETED,PENDING,CANCELLED;}
+public class Orders {
+    private int orderId;
+    private String customerName;
+    private Status order_status;
+    private int amount;
+
+    public Orders(int orderId, String customerName, Status order_status, int amount) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.order_status = order_status;
+        this.amount = amount;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public  Status getOrder_status() {
+        return order_status;
+    }
+
+    public void setOrder_status(Status order_status) {
+        this.order_status = order_status;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "\nOrder Id=" + orderId +
+                "\nCustomer Name='" + customerName +
+                "\nOrder_status=" + order_status +
+                "\nAmount=" + amount ;
+    }
+}
